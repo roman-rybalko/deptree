d00c467210c5f93be8dc7c6e83e708acae890d70
Roman Rybalko <devel@romanr.info>
2012-02-02 01:07:59 +0400

    fixed init_db for unexistent file

--- init_db.sh
+++ init_db.sh
@@ -4,5 +4,5 @@ name=test.sqlite
 idx=1
 while [ -e $name.$idx ]; do idx=`expr $idx + 1`; done
 while [ $idx -gt 1 ]; do nidx=`expr $idx - 1`; mv $name.$nidx $name.$idx; idx=$nidx; done
-mv $name $name.1
+[ ! -e $name ] || mv $name $name.1
 sqlite3 test.sqlite < schema.sql
